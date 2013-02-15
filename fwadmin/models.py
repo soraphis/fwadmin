10aea1e6aa300cdfce5324ab1f0805c94dc0d01e
Michael Vogt <mvo@ubuntu.com>
2013-02-15 08:32:14 +0100

    use models.GenericIPAddressField to support ipv6 as well

--- fwadmin/models.py
+++ fwadmin/models.py
@@ -6,14 +6,15 @@ class Port(models.Model):
     name = models.CharField(max_length=100)
     number = models.IntegerField()
     # TCP/UDP
-    #type = models.CharField(max_length=5)
+    type = models.CharField(max_length=5)
     def __unicode__(self):
         return "%s (%s)" % (self.name, self.number)
 
 
 class Host(models.Model):
     name = models.CharField(max_length=200)
-    ip = models.IPAddressField(max_length=100)
+    # supports both ipv4,ipv6
+    ip = models.GenericIPAddressField()
     active_until = models.DateField()
     owner = models.ForeignKey(User)
     open_ports = models.ManyToManyField(Port)
