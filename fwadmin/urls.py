9983d1a8e7e00ea00c6c267333a8cb6ef11f4eb6
Michael Vogt <mvo@ubuntu.com>
2013-02-15 21:51:09 +0100

    more ports

--- fwadmin/urls.py
+++ fwadmin/urls.py
@@ -1,18 +1,15 @@
 from django.conf.urls import patterns, url
-from django.views.generic import DetailView, ListView
 
-from fwadmin.models import Host
 
 urlpatterns = patterns('fwadmin.views',
     # user stuff
-    url(r'^new/$', 'new', name="new"),
+    url(r'^$', 'list', name='list'),
     url(r'^list/$', 'list', name='list'),
+
+    url(r'^new/$', 'new', name="new"),
     url(r'^edit/(?P<pk>\d+)/$', 'edit', name='edit'),
     url(r'^renew/(?P<pk>\d+)/$', 'renew', name='renew'),
     # admin stuff
     url(r'^admin-approve/(?P<pk>\d+)/$', 'approve', name='approve'),
     url(r'^admin-list-unapproved/$', 'list_unapproved', name='list_unapproved'),
-    #url(r'^(?P<detail_id>\d+)/$', 'detail'),
-    #url(r'^(?P<poll_id>\d+)/results/$', 'results'),
-    #url(r'^(?P<poll_id>\d+)/vote/$', 'vote'),
 )
