989eea26ac17aba1abb67f58aea2adaa3ef7fd41
Michael Vogt <mvo@debian.org>
2013-08-19 20:04:23 +0200

    use mysql in production

--- django_project/settings_production.py
+++ django_project/settings_production.py
@@ -1,1 +1,12 @@
 from settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fwadmin_db',
+        'USER': 'fwadmin',
+        'PASSWORD': open("db-password").read(),
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
