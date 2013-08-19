a11aed48d2d946992ee09881cfc2f4096126363e
Michael Vogt <mvo@debian.org>
2013-08-19 20:04:08 +0200

    prompt only once via env.sh for passwords

--- ansible/env.sh
+++ ansible/env.sh
@@ -1,8 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 
 BASEDIR="$(dirname $(pwd)/$(basename $0))"
 
-ANSIBLE_HOSTS="${BASEDIR}/ansible/inventory/ansible_hosts"
+ANSIBLE_HOSTS="${BASEDIR}/inventory/ansible_hosts"
 export ANSIBLE_HOSTS
 
 echo "Using ansible hosts $ANSIBLE_HOSTS"
+
+mkdir -p "$BASEDIR/non-git-files"
+LDAP_PW_FILE="$BASEDIR/non-git-files/ldap-password"
+if [ ! -e "$LDAP_PW_FILE" ]; then
+    # read -s is a bashism
+    read -s -p 'Please enter the ldap password: ' LDAP_PW
+    echo "$LDAP_PW" > $LDAP_PW_FILE
+    echo ""
+fi
+
+DB_PW_FILE="$BASEDIR/non-git-files/db-password"
+if [ ! -e "$DB_PW_FILE" ]; then
+    # read -s is a bashism
+    read -s -p 'Please enter the db password: ' DB_PW
+    echo "$DB_PW" > $DB_PW_FILE
+    echo ""
+fi
