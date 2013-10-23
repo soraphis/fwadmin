16e222366ce2a44469b4d7f591d35feaefbaae2d
Michael Vogt <mvo@debian.org>
2013-10-23 15:39:49 +0200

    make work with our LDAP

--- django_project/settings.py
+++ django_project/settings.py
@@ -207,9 +207,9 @@ AUTH_LDAP_BIND_PASSWORD = open(os.path.join(os.path.dirname(__file__),
 #AUTH_LDAP_SERVER_URI = "ldaps://saul.uni-trier.de:636"
 
 # custom search as the DN uses the full account name
-AUTH_LDAP_USER_SEARCH = LDAPSearch('cn=Users,dc=uni-trier,dc=de', 
+AUTH_LDAP_USER_SEARCH = LDAPSearch('CN=Users,DC=uni-trier,DC=de', 
                                    ldap.SCOPE_SUBTREE, 
-                                   '(sAMAccountName=%(user)s)')
+                                   '(CN=%(user)s)')
 
 # needed to make ActiveDirectory work
 AUTH_LDAP_CONNECTION_OPTIONS = {
@@ -224,7 +224,7 @@ AUTH_LDAP_USER_ATTR_MAP = {
 # this will crash and burn if there is no such group
 AUTH_LDAP_USER_FLAGS_BY_GROUP = {
     # auto allow access to admin interface for our group
-    "is_staff": "CN=G-zentrale-systeme,CN=Users,DC=emailtest,DC=uni-trier,DC=de",
+    "is_staff": "CN=G-zentrale-systeme,CN=Users,DC=uni-trier,DC=de",
 }
 # limit login to "Mitarb", (not needed, see fwadmin/view.py)
 #AUTH_LDAP_REQUIRE_GROUP = "cn=Mitarb,CN=Users,DC=emailtest,dc=uni-trier,dc=de"
@@ -234,7 +234,7 @@ AUTH_LDAP_MIRROR_GROUPS = True
 AUTH_LDAP_GROUP_TYPE = ActiveDirectoryGroupType()
 #AUTH_LDAP_CACHE_GROUPS = True
 #AUTH_LDAP_GROUP_CACHE_TIMEOUT = 3600
-AUTH_LDAP_GROUP_SEARCH = LDAPSearch('CN=Users,DC=emailtest,DC=uni-trier,DC=de',
+AUTH_LDAP_GROUP_SEARCH = LDAPSearch('CN=Users,DC=uni-trier,DC=de',
                                     ldap.SCOPE_SUBTREE,
                                     "(objectClass=group)")
 
