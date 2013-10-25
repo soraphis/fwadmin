9fc0d48d89b8a4cfd3402b5fe50b4fb7ab6d01a9
Michael Vogt <mvo@debian.org>
2013-10-25 10:34:00 +0200

    use postgresql instead of mysql

--- django_project/settings_production.py
+++ django_project/settings_production.py
@@ -6,7 +6,7 @@ TEMPLATE_DEBUG = DEBUG
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'fwadmin_db',
         'USER': 'fwadmin',
         'PASSWORD': open(os.path.join(
