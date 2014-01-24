ec959e3ee3234e6eaf6ef1f4425d84b4781c7d6c
Peter Golm <golm.peter@gmail.com>
2014-01-24 12:11:04 +0100

    add some tests for static rules

--- fwadmin/tests/test_models.py
+++ fwadmin/tests/test_models.py
@@ -1,8 +1,28 @@
 
 from django.utils import unittest
 from fwadmin.models import (
+    StaticRule,
     Host,
 )
+
+
+class StaticRuleTestCase(unittest.TestCase):
+
+    def test_defaults(self):
+        rule = StaticRule()
+
+        self.assertEqual(rule.type, StaticRule.HEADER)
+        self.assertEqual(rule.fw_type, StaticRule.FW_TYPE_CHOICES[0][0])
+        self.assertEqual(rule.text, "")
+
+    def test_rule(self):
+        rule = StaticRule()
+        rule.save()
+
+        rule = StaticRule.objects.get(id=rule.id)
+        self.assertEqual(rule.type, StaticRule.HEADER)
+        self.assertEqual(rule.fw_type, StaticRule.FW_TYPE_CHOICES[0][0])
+        self.assertEqual(rule.text, "")
 
 
 class HostTestCase(unittest.TestCase):
