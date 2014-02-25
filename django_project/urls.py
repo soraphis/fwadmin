57c07de8540d90df1a8f15e8199ecd861c09cb33
oliver <soraphis@googlemail.com>
2014-02-25 10:18:19 +0100

    activate i18n urls

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import RedirectView
+from django.conf.urls.i18n import i18n_patterns
 
 # enable admin interface
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = i18n_patterns('',
     # Examples:
     # url(r'^$', 'django_project.views.home', name='home'),
     # url(r'^django_project/', include('django_project.foo.urls')),
