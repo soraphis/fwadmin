70f4f081236e026b64d83a8bfddc580f366ca7fb
Michael Trappen <mtrappen@gmail.com>
2014-03-10 16:08:52 +0100

    added test cases for validate_from_net

--- fwadmin/tests/test_validators.py
+++ fwadmin/tests/test_validators.py
@@ -1,6 +1,6 @@
 import unittest
 from django.core.exceptions import ValidationError
-from fwadmin.validators import validate_port
+from fwadmin.validators import validate_port, validate_from_net
 
 
 class ValidatorTests(unittest.TestCase):
@@ -13,7 +13,6 @@ class ValidatorTests(unittest.TestCase):
             '33', '44', '1024', '5000',
             ' 42     ', '    42', '1     - 5',
             '11-11',
-
         )
         # expected to raise ValidationError
         invalid = (
@@ -30,3 +29,22 @@ class ValidatorTests(unittest.TestCase):
         for case in invalid:
             with self.assertRaises(ValidationError):
                 validate_port(case)
+
+    def test_validate_from_net(self):
+        # expected to be valid
+        valid = (
+            '127.0.0.1', '0.0.0.0', '255.255.255.255',
+            '/23',
+        )
+
+        # expected to raise ValidationError
+        invalid = (
+            '123.123.123', '777.0.0.1', 'spam.0.0.1', '-1.0.0.1',
+            '/34', '/-1', '/spam'
+        )
+
+        for case in valid:
+            self.assertEqual(validate_from_net(case), None)
+        for case in invalid:
+            with self.assertRaises(ValidationError):
+                validate_from_net(case)
